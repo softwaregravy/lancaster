80588b2419fd6a20b3e2f88dc92f4104adb957f9
John Hinnegan <johnhinnegan@Johns-MacBook-Pro-2.local>
2016-05-03 15:13:46 -0400

    specs passing again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 
@@ -14,9 +15,10 @@ class UsersController < ApplicationController
   end
 
   # GET /users/new
-  def new
-    @user = User.new
-  end
+  # created via devise
+  #def new
+    #@user = User.new
+  #end
 
   # GET /users/1/edit
   def edit
@@ -24,19 +26,19 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
-    @user = User.new(user_params)
+  #def create
+    #@user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    #respond_to do |format|
+      #if @user.save
+        #format.html { redirect_to @user, notice: 'User was successfully created.' }
+        #format.json { render :show, status: :created, location: @user }
+      #else
+        #format.html { render :new }
+        #format.json { render json: @user.errors, status: :unprocessable_entity }
+      #end
+    #end
+  #end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,16 +2,33 @@
 #
 # Table name: users
 #
-#  id           :integer          not null, primary key
-#  email        :string
-#  phone_number :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id                     :integer          not null, primary key
+#  phone_number           :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default("0"), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 
 FactoryGirl.define do 
   factory :user do 
-    sequence :email do |n| "email#{n}@example.com" end
+    email { Faker::Internet.email }
+    password "password"
+    password_confirmation "password"
+    confirmed_at Date.today
+
     phone_number "555-555-5555"
   end
 end
