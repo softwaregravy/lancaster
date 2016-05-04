0b959b4b436c6d9dcb30880883cd16710ebfcb06
John Hinnegan <johnhinnegan@Johns-MacBook-Pro-2.local>
2016-05-04 17:02:53 -0400

    fix comments

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,9 +2,9 @@
 # put your own credentials here
 account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_ACCOUNT_AUTH_TOKEN']
-# This is Twilio's test number for sending SMS messages
-# https://www.twilio.com/docs/api/rest/test-credentials
 if Rails.env.test?
+  # This is Twilio's test number for sending SMS messages
+  # https://www.twilio.com/docs/api/rest/test-credentials
   SmsMessage.from_number = '+15005550006'
 else
   SmsMessage.from_number = ENV['TWILIO_FROM_NUMBER']
