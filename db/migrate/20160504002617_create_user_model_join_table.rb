53e5079053fa6d68c4ff246c9320c6d466140dc0
John Hinnegan <johnhinnegan@Johns-MacBook-Pro-2.local>
2016-05-03 21:37:52 -0400

    code review tweaks

--- db/migrate/20160504002617_create_user_model_join_table.rb
+++ db/migrate/20160504002617_create_user_model_join_table.rb
@@ -2,7 +2,7 @@ class CreateUserModelJoinTable < ActiveRecord::Migration
   def change
     create_join_table :users, :roles do |t|
       t.index [:user_id, :role_id]
-      # t.index [:role_id, :user_id]
+      t.index [:role_id, :user_id]
       t.timestamps null: false
     end
   end
