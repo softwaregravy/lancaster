f28c0340a70e94b7073a61a1c42c83870193408a
John Hinnegan <johnhinnegan@Johns-MacBook-Pro-2.local>
2016-05-07 15:58:52 -0400

    Set up sendgrid

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -10,10 +10,24 @@ gmail_settings = {
   enable_starttls_auto: true
 }
 
-ActionMailer::Base.smtp_settings = gmail_settings
+sendgrid_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = sendgrid_settings
+else
+  ActionMailer::Base.smtp_settings = gmail_settings
+end
 
 #TODO currently restrict any live emails. When we're ready, we need to open up for production to actually send emails
-if true
-# if Rails.env.development? || Rails.env.test?
+unless Rails.env.production?
   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)                                                                                                                                                                                                                                                                                                     
 end
