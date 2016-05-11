7b8fc1c6a5e2628baaa293cab231339003b15dd3
John Hinnegan <johnhinnegan@Johns-MacBook-Pro-2.local>
2016-05-11 16:22:44 -0400

    config tickwork

--- config/initializers/tick.rb
+++ config/initializers/tick.rb
@@ -3,6 +3,8 @@ require 'tickwork'
 module Tickwork
   configure do |config|
     config[:data_store] = AwsTickwork::DbDataStore
+    config[:tick_size] = 60 # 1 minute                                                                                                                                                                                                                                                                                                                                         
+    config[:max_ticks] = ENV['TICKWORK_MAX_TICKS'] || 10 
   end
   every 1.hour, 'refreshing_feeds' do
     RefreshFeedsWorker.perform_async
